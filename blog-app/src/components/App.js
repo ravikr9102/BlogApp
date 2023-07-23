96f4b168fc933c9945838accc51b8fab28b7c9af
Ravi kumar <rkravikr9102@gmail.com>
2023-07-23 10:35:45 +0530

    Done Day 1 for Blog App

--- blog-app/src/components/App.js
+++ blog-app/src/components/App.js
@@ -1,24 +1,30 @@
-import React from "react";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './Header';
+import Home from './Home';
+import SinglePost from './SinglePost';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import NoMatch from './NoMatch';
 
-class App extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            articles: null,
-        }
-    }
-    // componentDidMount() {
-    //     fetch()
-    //       .then((res) => res.json())
-    //       .then((articles) => this.setState({ articles }));
-    //   }
-    render(){
-        return(
-            <React.Fragment>
-            <h1>Hello Blog App</h1>
-            </React.Fragment>
-        )
-    }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/articles/:slug" element={<SinglePost />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path='*' element={<NoMatch />} />
+        </Routes>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
